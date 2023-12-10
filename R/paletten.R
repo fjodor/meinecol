c063f822ec3c528d29c43e580defd9fdfceedcf0
fjodor <wolf.riepl@statistik-dresden.de>
2023-12-10 18:24:04 +0100

    Farbskalen anonymisiert

--- R/paletten.R
+++ R/paletten.R
@@ -1,5 +1,5 @@
 my_paletten <- list(
-  `gruen` = dezim_cols("Caribbean Current", "Dartmouth green"),
-  `drei`  = dezim_cols("Caribbean Current", "Dartmouth green", "Tangerine"),
-  `alle`   = dezim_cols()
+  `gruen` = my_cols("Caribbean Current", "Dartmouth green"),
+  `drei`  = my_cols("Caribbean Current", "Dartmouth green", "Tangerine"),
+  `alle`   = my_cols()
 )
